78c68a5c6dce4ae37a04ab75a3f6bd9254ef11ec
Quezon Province Programmer, Developers <quezon.province.pd@gmail.com>
2025-09-23 23:19:18 +0800

    Updated

--- Source/BikeTracker/PinConfig.h
+++ Source/BikeTracker/PinConfig.h
@@ -5,7 +5,7 @@
 #define PINCONFIG_H
 
 // NodeMCU v3 1.0: Define pins for software serial ports
-// GPS: RX=D2, TX=D3; GSM: RX=D5, TX=D6
+// GPS: RX=D3, TX=D2; GSM: RX=D6, TX=D5
 #define GPS_RX_PIN D3
 #define GPS_TX_PIN D2
 #define GSM_RX_PIN D6
